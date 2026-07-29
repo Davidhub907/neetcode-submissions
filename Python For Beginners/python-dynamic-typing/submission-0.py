2231f86f5a9016b6a9b2945ec39718f3d265b0f3
David Gray <davidagray2005@gmail.com>
2026-07-29 10:29:02 -0800

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 1
+print(type(variable))
+variable = 0.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Hello"
+print(type(variable))
+variable = [1,3,3]
+print(type(variable))
